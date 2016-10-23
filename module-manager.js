f3d92d3048d647d30c388e549202cfc5afaeebd7
Manoj Singh Negi <justanothermanoj@gmail.com>
2016-10-23 11:34:50 +0530

    added dummy function

--- module-manager.js
+++ module-manager.js
@@ -1,1 +1,14 @@
-console.log('I am a module manager')
+var Manager = (
+	function Manager() {
+
+		var modules = {
+			dummy: function(name) {
+				console.log("Hello My name is " + name)
+			}
+		};
+
+		return modules;
+	}
+)();
+
+Manager.dummy('Manoj');
